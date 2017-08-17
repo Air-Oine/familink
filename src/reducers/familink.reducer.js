a18a16f4fd5e8dff4d8eaab352053419bcd9416f
Erjon Lamy <erjon.lamy@outlook.fr>
2017-08-17 11:14:29 +0200

    begin implementation of redux

---
+++ src/reducers/familink.reducer.js
@@ -0,0 +1,18 @@
+import {ADD_TOKEN} from '../actions/familink.actions';
+
+export const initialState = {
+  token: '',
+};
+
+export default function familinkReducer(state = initialState, action) {
+  switch (action.type) {
+    case ADD_TOKEN: {
+      return {
+        ...state,
+        token: state.token,
+      },
+    }
+    default:
+      return state;
+  }
+}
