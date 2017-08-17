3fd0a2a7c557ea2f5b28ee868853c06401434a80
Admin <romain-lalesnel@hotmail.fr>
2017-08-17 11:20:26 +0200

    Classe pour Bdd locale ajouté

---
+++ src/asyncStorage.js
@@ -0,0 +1,56 @@
+import { AsyncStorage } from 'react-native';
+
+export default class Storage {
+  // return -1 si une erreur est survenu
+  static async getItem(item) {
+    try {
+      const value = await AsyncStorage.getItem(item);
+      return value;
+    } catch (error) {
+      return -1;
+    }
+  }
+  // return 1 si l'enregistrement est OK
+  // return -1 si une erreur est survenu et l'enregistrement pas OK
+  static async setItem(item, value) {
+    try {
+      await AsyncStorage.setItem(item, value);
+      return 1;
+    } catch (error) {
+      return -1;
+    }
+  }
+}
+
+// Exemple pour l'application, il faut use le state 
+
+// export default class App extends Component {
+
+//   componentWillMount() {
+//     Storage.setItem('Keytest', 'Oulala');
+//   }
+//   constructor(props) {
+//     super(props);
+//      this.state = {
+//         token: null,    
+//     }
+//   }
+//   async getitem() {
+//     const value = await Storage.getItem('Keytest');
+//     this.setState({
+//           token: value,
+//         })
+//     return value;
+//   }
+//   render() {
+//     this.getitem();
+//     const item = this.state.token;
+//     console.log(this.state.token);
+//     return (
+//       <View>
+//         <Text> {item}
+//         </Text>
+//       </View>
+//     );
+//   }
+// }
