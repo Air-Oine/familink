72c7a96d5b895410366f4f05166fbfde33cfbed2
Seymour_le_Grand <a@b.c>
2017-08-18 11:05:55 +0200

    commir pour eau gazeuse

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -7,32 +7,49 @@ import {
   View,
   Image,
   Text,
+  Button
 } from 'react-native';
-import { CONTACTLIST_SCENE_NAME } from '../screens/ContactListScreen';
-const window = Dimensions.get('window');
-const uri = 'https://pickaface.net/gallery/avatar/Opi51c74d0125fd4.png';
 
+const styles = StyleSheet.create({
+  menu: {
+    flex: 1,
+    width: window.width,
+    height: window.height,
+    backgroundColor: 'gray',
+    padding: 20,
+  },
+  avatarContainer: {
+    marginBottom: 20,
+    marginTop: 20,
+  },
+  avatar: {
+    width: 48,
+    height: 48,
+    borderRadius: 24,
+    flex: 1,
+  },
+  name: {
+    position: 'absolute',
+    left: 70,
+    top: 20,
+  },
+  item: {
+    fontSize: 14,
+    fontWeight: '200',
+    paddingTop: 5,
+  },
+});
+export default function Menu({ onItemSelected }) {
 
-export default function Menu() {
   return (
     <ScrollView scrollsToTop={false} style={styles.menu}>
-      <View style={styles.avatarContainer}>
-        <Image
-          style={styles.avatar}
-          source={{ uri }}
-        />
-        <Text style={styles.name}>Your name</Text>
-      </View>
 
-      <Text>
-        Accueil
-      </Text>
 
-      <Text>
+      <Text
+        onPress={() => onItemSelected('Annuaire')}
+        style={styles.item}
+      >
         Annuaire
-      </Text>
-      <Text>
-        Déconnexion
       </Text>
     </ScrollView>
   );
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -2,46 +2,107 @@ import React, { Component } from 'react';
 import {
   ScrollView,
   Text,
+  StyleSheet,
+  TouchableOpacity,
+  Image,
+  View,
 } from 'react-native';
 import Menu from '../components/Menu';
-
+import SideMenu from 'react-native-side-menu';
+import {CONTACTLIST_SCENE_NAME} from '../screens/ContactListScreen'
 export const HOME_SCENE_NAME = 'HOME_SCENE';
 
 
 const image = require('../../assets/menu.png');
+const styles = StyleSheet.create({
+  button: {
+    position: 'absolute',
+    top: 20,
+    padding: 10,
+  },
+  caption: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    alignItems: 'center',
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
 
 export default class HomeScreen extends Component {
-  
-  
+
   static navigationOptions = {
     title: 'Home',
   };
   constructor(props) {
     super(props);
 
+    this.navigate = this.props.navigation.navigate;
+    this.navigateToContactList = this.navigateToContactList.bind(this);
+    this.onMenuItemSelected = this.onMenuItemSelected.bind(this);
     this.toggle = this.toggle.bind(this);
 
     this.state = {
       isOpen: false,
     };
-  }
-  /* constructor(props) {
-    super(props);
-  } */
-  
+  };
+
+  navigateToContactList(){
+    this.navigate(CONTACTLIST_SCENE_NAME);
+  };
+
   toggle() {
     this.setState({
       isOpen: !this.state.isOpen,
     });
+  };
+
+  onMenuItemSelected(item){
+    this.toggle();
+    this.navigateToContactList()
   }
+    
+
+  updateMenuState(isOpen) {
+    this.setState({ isOpen });
+  };
 
   render() {
+    const menu = <Menu onItemSelected={this.onMenuItemSelected} />;
     return (
-      <ScrollView>
-        <Text>
-          ceci est la page home
-        </Text>
-      </ScrollView>
+      <SideMenu
+        menu={menu}
+        isOpen={this.state.isOpen}
+        onChange={isOpen => this.updateMenuState(isOpen)}
+      >
+        <View style={styles.container}>
+          <Text>ceci est la page home </Text>
+        </View>
+        <TouchableOpacity
+          onPress={this.toggle}
+          style={styles.button}
+        >
+          <Image
+            source={image}
+            style={{ width: 32, height: 32 }}
+          />
+        </TouchableOpacity>
+        
+      </SideMenu>
     );
   }
 }
