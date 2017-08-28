c3d70b207585deb3a4b386cd308a7981fe4d3dd8
Seymour_le_Grand <a@b.c>
2017-08-28 09:50:10 +0200

    styles accueil

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -6,7 +6,7 @@ import {
 } from 'react-native';
 
 import { connect } from 'react-redux';
-import { Container, Content, Icon, Grid, Col } from 'native-base';
+import { Container, Content, Icon, Grid, Col, Row } from 'native-base';
 import { styles } from '../style';
 import HeaderBar from '../components/HeaderBar';
 import { CONTACTLIST_SCENE_NAME } from './ContactListScreen';
@@ -35,45 +35,47 @@ class HomeScreen extends Component {
     return (
       <Container>
         <HeaderBar navigation={navigation} title={AppString.homePageName} />
-        <Content>
+        <Content style={styles.accueilMargin}>
           <Grid>
-            <Col>
-              <TouchableOpacity onPress={() => { navigation.navigate(CONTACTLIST_SCENE_NAME); }} >
-                <Text style={styles.MenuText}>
-                  {AppString.homescreenAnnuaire}
-                </Text>
-                <Image source={annuaire} />
-              </TouchableOpacity>
-            </Col>
-            <Col>
-              <TouchableOpacity onPress={this.navigateToContactList}>
-                
-                <Text style={styles.MenuText}>
-                  {AppString.homescreenHumeur}
-                </Text>
-                <Image source={smile} />
-              </TouchableOpacity>
-            </Col>
-          </Grid>
-          <Grid>
-            <Col>
-              <TouchableOpacity onPress={this.navigateToContactList}>
-                
-                <Text style={styles.MenuText}>
-                  {AppString.homescreenConfig}
-                </Text>
-                <Image source={config} />
-              </TouchableOpacity>
-            </Col>
-            <Col>
-              <TouchableOpacity onPress={this.navigateToContactList}>
-                
-                <Text style={styles.MenuText}>
-                  {AppString.homescreenProfil}
-                </Text>
-                <Image source={profil} />
-              </TouchableOpacity>
-            </Col>
+            <Row size={1}>
+              <Col>
+                <TouchableOpacity onPress={() => { navigation.navigate(CONTACTLIST_SCENE_NAME); }} >
+                  <Text style={styles.MenuText}>
+                    {AppString.homescreenAnnuaire}
+                  </Text>
+                  <Image source={annuaire} />
+                </TouchableOpacity>
+              </Col>
+              <Col>
+                <TouchableOpacity onPress={this.navigateToContactList}>
+
+                  <Text style={styles.MenuText}>
+                    {AppString.homescreenHumeur}
+                  </Text>
+                  <Image source={smile} />
+                </TouchableOpacity>
+              </Col>
+            </Row>
+            <Row size={1}>
+              <Col>
+                <TouchableOpacity onPress={this.navigateToContactList}>
+
+                  <Text style={styles.MenuText}>
+                    {AppString.homescreenConfig}
+                  </Text>
+                  <Image source={config} />
+                </TouchableOpacity>
+              </Col>
+              <Col>
+                <TouchableOpacity onPress={this.navigateToContactList}>
+
+                  <Text style={styles.MenuText}>
+                    {AppString.homescreenProfil}
+                  </Text>
+                  <Image source={profil} />
+                </TouchableOpacity>
+              </Col>
+            </Row>
           </Grid>
         </Content>
       </Container>
