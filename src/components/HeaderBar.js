dd7eed65ffce7b5b6f8e749cc91df2439fab9d06
Erwan Masson <airone.masson@gmail.com>
2017-08-28 17:51:24 +0200

    Header bar show correctly long title

--- src/components/HeaderBar.js
+++ src/components/HeaderBar.js
@@ -61,14 +61,19 @@ export default function HeaderBar(props) {
     deleteButton = renderButton(props.deleteOnPress, 'trash');
   }
 
+  // Handle long title
+  const bodySize = {
+    flex: props.title.length > 10 ? 9 : 4,
+  };
+
   return (
     <Header androidStatusBarColor={darkPrimaryColor} style={styles.headerBarHeader}>
       {/* BURGER MENU BUTTON */}
-      <Left>
+      <Left style={styles.flex1}>
         {menuButton}
       </Left>
       {/* PAGE TITLE */}
-      <Body>
+      <Body style={bodySize}>
         <Title style={styles.headerBarTitle}>
           {props.title}
         </Title>
